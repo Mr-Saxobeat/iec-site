955a506e2902a54ae72bc3fde65847fac9ed45e9
Mr-Saxobeat <weiglasribeiro@gmail.com>
2020-08-06 12:31:17 -0300

    Cria view para download de serie historica e api para dados do ProjETA

--- vismet/urls.py
+++ vismet/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.VisMetView, name='vismet'),
     path('xavierweathergeojson/', views.XavierStationWeatherGeoJson.as_view(), name="xavierweathergeojson"),
     path('ajaxrequest/', views.ajaxrequest, name='ajaxrequest'),
+    path('apiprojeta/', views.ApiProjeta, name='apiprojeta'),
+    path('download/<str:variable>/<int:station_id>/<int:start_day>-<int:start_month>-<int:start_year>/<int:final_day>-<int:final_month>-<int:final_year>/', views.Download, name='download'),
 ]
--- vismet/views.py
+++ vismet/views.py
@@ -4,6 +4,7 @@ from .models import XavierWeatherStation, StationData
 from django.http import HttpResponse, JsonResponse
 import json
 import datetime
+import requests
 from django.core import serializers
 from rest_framework import serializers as rest_serializers
 
@@ -13,6 +14,17 @@ def VisMetView(request):
 class XavierStationWeatherGeoJson(GeoJSONLayerView):
     model = XavierWeatherStation
     properties = ('popup_content', 'stationId', 'name', 'state', 'omm_code', 'latitude', 'longitude')
+
+def ApiProjeta(request):
+    climate_models = requests.get("https://projeta.cptec.inpe.br/api/v1/public/models").json()
+    intervals = requests.get("https://projeta.cptec.inpe.br/api/v1/public/intervals").json()
+    variables = requests.get("https://projeta.cptec.inpe.br/api/v1/public/variables").json()
+    return render(request, 'vismet/apiprojeta.html',
+                            {
+                                'climate_models': climate_models["data"],
+                                'intervals': intervals["data"],
+                                'variables': variables["data"]
+                            })
 
 def ajaxrequest(request):
     stationId = request.GET.get('stationId')
@@ -40,3 +52,17 @@ def ajaxrequest(request):
 
     return HttpResponse(myData, content_type="application/json")
     # return JsonResponse(myData, safe=False)
+
+def Download(request, variable, station_id, start_day, start_month, start_year, final_day, final_month, final_year):
+    sDate = datetime.date(start_year, start_month, start_day)
+    eDate = datetime.date(final_year, final_month, final_day)
+
+    station = XavierWeatherStation.objects.get(station_id=station_id)
+
+    station_timestamp = station.data.filter(date__gte=sDate, date__lte=eDate).order_by('date')
+    myData = serializers.serialize('json', station_timestamp)
+
+    response = HttpResponse(myData, content_type="applications/json")
+    # response = JsonResponse(myData, safe=False)
+    # response['Content-Disposition'] = "attachment; filename=%s" %'time_series.json'
+    return response
