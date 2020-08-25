6d7b979f6d3e0569928a1143c8daa72a4821b49d
Mr-Saxobeat <weiglasribeiro@gmail.com>
2020-08-25 11:52:13 -0300

    Corrige urls

--- vismet/urls.py
+++ vismet/urls.py
@@ -5,11 +5,11 @@ app_name = 'vismet'
 
 urlpatterns = [
     path('', views.VisMetView, name='vismet'),
-    path('api/xavierstations/', views.XavierStationWeatherGeoJson.as_view(), name="xavier-station-geojson"),
-    path('cities/', views.CityGeoJson.as_view()),
+    path('api/xavierstations/', views.XavierStationWeatherGeoJson.as_view(), name='xavier_stations_layer'),
+    path('api/cities/', views.CityGeoJson.as_view(), name='cities_layer'),
+    path('api/pixels/', views.PixelGeoJson.as_view(), name='pixels_layer'),
     path('ajaxrequest/', views.ajaxrequest, name='ajaxrequest'),
     path('api/xavier/<str:variable>/<int:station_id>/<int:start_day>-<int:start_month>-<int:start_year>/<int:final_day>-<int:final_month>-<int:final_year>/', views.ApiXavier, name='apixavier'),
     path('api/pixeldata/', views.PixelDataView, name='pixeldata'),
     path('api/pixeldata/<int:lat>/<int:lng>/<int:start_day>-<int:start_month>-<int:start_year>/<int:final_day>-<int:final_month>-<int:final_year>/', views.PixelData2View),
-    path('pixel/', views.PixelGeoJson.as_view()),
 ]
