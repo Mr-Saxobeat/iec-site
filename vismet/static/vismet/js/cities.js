628828450da066a26d26725c89cdf6a8fb182265
Mr-Saxobeat <weiglasribeiro@gmail.com>
2020-08-27 17:21:18 -0300

    Add função para atualizar grafico

--- vismet/static/vismet/js/cities.js
+++ vismet/static/vismet/js/cities.js
@@ -2,9 +2,38 @@ cities_layer_style = {
   color: 'red',
 };
 
+var i = 0;
+
+function refreshChart(city_id){
+  $.getJSON("http://127.0.0.1:8000/api/pixeldata/" + city_id + "/1-1-1960/1-1-1960/",
+            function (data) {
+              dates = []
+              prec = []
+
+              data.forEach(dt => {
+                dates.push(dt.date);
+                prec.push(dt.preciptation);
+              });
+
+              // if(i == 0){
+              //   console.log(dates);
+              //   console.log(prec);
+              //   i = 1;
+              // }
+
+              removeData(chart);
+              addData(chart, dates, prec);
+              chart.update();
+            });
+
+}
+
+var i = 0;
+
 function onEachFeature(feature, layer) {
   var popupContent = "Nome:" + feature.properties.nome;
   layer.bindPopup(popupContent);
+  layer.on("click", function(){refreshChart(feature.id);});
 }
 
 var cities_layer = L.geoJson([], {
