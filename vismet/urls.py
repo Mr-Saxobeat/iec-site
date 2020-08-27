429934def58cee1608c3ddeba7c624e4e4b3feaf
Mr-Saxobeat <weiglasribeiro@gmail.com>
2020-08-27 17:18:58 -0300

    Corrige urls para variaveis das apis

--- vismet/urls.py
+++ vismet/urls.py
@@ -6,14 +6,16 @@ app_name = 'vismet'
 urlpatterns = [
     path('', views.VisMetView, name='vismet'),
 
-    path('api/xavierstations/', views.Api_XavierStations.as_view(), name='xavier_stations_layer'),
+    path('api/xavierstations/', views.Api_XavierStations.as_view(), name='xavier_stations'),
     path('api/xavierstations/<int:omm_code>/<int:start_day>-<int:start_month>-<int:start_year>/<int:final_day>-<int:final_month>-<int:final_year>/',
          views.Api_XavierStations_TimeStamp,
          name='xavier_stations_timestamp'),
-         
-    path('api/cities/', views.CityGeoJson.as_view(), name='cities_layer'),
-    path('api/pixels/', views.PixelGeoJson.as_view(), name='pixels_layer'),
+
+    path('api/pixels/', views.Api_Pixel.as_view(), name='pixels'),
+    path('api/pixels/<int:pk>/<int:start_day>-<int:start_month>-<int:start_year>/<int:final_day>-<int:final_month>-<int:final_year>/',
+         views.Api_Pixel_Data,
+         name='pixel_data'),
+
+    path('api/cities/', views.CityGeoJson.as_view(), name='cities'),
     path('ajaxrequest/', views.ajaxrequest, name='ajaxrequest'),
-    path('api/pixeldata/', views.PixelDataView, name='pixeldata'),
-    path('api/pixeldata/<int:pixel_id>/<int:start_day>-<int:start_month>-<int:start_year>/<int:final_day>-<int:final_month>-<int:final_year>/', views.PixelDataView),
 ]
