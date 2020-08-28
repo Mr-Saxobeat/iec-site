c9ab0176ba1351b7ccd1801f31d1ee8aa4bb9f45
Mr-Saxobeat <weiglasribeiro@gmail.com>
2020-08-28 16:33:37 -0300

    Corrige urls e cria url para Cities_Data

--- vismet/urls.py
+++ vismet/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
 
     path('api/xavierstations/', views.Api_XavierStations.as_view(), name='xavier_stations'),
     path('api/xavierstations/<int:omm_code>/<int:start_day>-<int:start_month>-<int:start_year>/<int:final_day>-<int:final_month>-<int:final_year>/',
-         views.Api_XavierStations_TimeStamp,
+         views.Api_XavierStations_Data,
          name='xavier_stations_timestamp'),
 
     path('api/pixels/', views.Api_Pixel.as_view(), name='pixels'),
@@ -16,6 +16,11 @@ urlpatterns = [
          views.Api_Pixel_Data,
          name='pixel_data'),
 
-    path('api/cities/', views.CityGeoJson.as_view(), name='cities'),
+    path('api/cities/', views.Api_Cities.as_view(), name='cities'),
+    path('api/cities/<str:name>/<int:start_day>-<int:start_month>-<int:start_year>/<int:final_day>-<int:final_month>-<int:final_year>/',
+         views.Api_Cities_Data,
+         name='cities_data'),
+
+
     path('ajaxrequest/', views.ajaxrequest, name='ajaxrequest'),
 ]
