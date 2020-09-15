e075e588a351cf0443ce46206845ba3c88b80b73
Mr-Saxobeat <weiglasribeiro@gmail.com>
2020-09-14 21:34:29 -0300

    Cria arquivo para funcoes das estacoes xavier

--- vismet/static/vismet/js/xavier-stations.js
+++ vismet/static/vismet/js/xavier-stations.js
@@ -47,3 +47,58 @@ var overlays = {
 };
 
 control.addOverlay(xaviewrWeatherStation, "Estações Xavier");
+
+var btn_submit = $("#btn_submit");
+
+btn_submit.click(function(){
+
+  var ommCode = $("#input_ommcode").val();
+  var startDate = $("#startDate").val();
+  var finalDate = $("#finalDate").val();
+  var variable = $("#variable").val();
+
+  for(i = 0; i < 3; i++){
+    startDate = startDate.replace("/", "-");
+    finalDate = finalDate.replace("/", "-");
+  }
+
+  $.getJSON(url_api_xavierstation + "json" + "/" + ommCode + "/" + startDate + "/" + finalDate,
+    function(data_response){
+      console.log(data_response);
+
+      removeData(chart);
+
+      var value;
+
+      data_response.forEach((dt, i) => {
+        chart.data.labels.push(dt.fields.date);
+
+        value = dt.fields[variable];
+
+        if(value == 0 || value == "NaN" || value == "-9999"){
+          chart.data.datasets[0].data.push(null);
+        }
+        else{
+          chart.data.datasets[0].data.push(value);
+        }
+      });
+
+      chart.update();
+  })
+})
+
+var btn_download = $("#btn_download");
+btn_download.click(function(){
+  var ommCode = $("#input_ommcode").val();
+  var startDate = $("#startDate").val();
+  var finalDate = $("#finalDate").val();
+  var variable = $("#variable").val();
+
+  for(i = 0; i < 3; i++){
+    startDate = startDate.replace("/", "-");
+    finalDate = finalDate.replace("/", "-");
+  }
+  
+  window.location = url_api_xavierstation + "csv" + "/" + ommCode + "/" + startDate + "/" + finalDate;
+
+})
