9927477e999d085589eb432518055c44a967b10c
Mr-Saxobeat <weiglasribeiro@gmail.com>
2020-09-14 21:32:49 -0300

    Implementa funcoes para adicionar layer de cidades e highlight selections

--- vismet/static/vismet/js/cities.js
+++ vismet/static/vismet/js/cities.js
@@ -1,4 +1,4 @@
-cities_layer_style = {
+style = {
   color: 'red',
 };
 
@@ -15,12 +15,6 @@ function refreshChart(city_id){
                 prec.push(dt.preciptation);
               });
 
-              // if(i == 0){
-              //   console.log(dates);
-              //   console.log(prec);
-              //   i = 1;
-              // }
-
               removeData(chart);
               addData(chart, dates, prec);
               chart.update();
@@ -28,16 +22,78 @@ function refreshChart(city_id){
 
 }
 
-var i = 0;
+function highlightFeature(e) {
+  var layer = e.target;
+
+  layer.setStyle({
+    color: 'green',
+  });
+
+  if(!L.Browser.ie && !L.Browser.opera && !L.Browser.edge){
+    layer.bringToFront();
+  }
+}
+
+function resetHighlight(e) {
+  if(e.target != oldLayer){
+    cities_layer.resetStyle(e.target);
+  }
+}
+
+var oldLayer = null;
+
+function selectFeature(e) {
+
+  if(oldLayer){
+    cities_layer.resetStyle(oldLayer);
+  }
+
+  var layer = e.target;
+
+  layer.setStyle({
+    color: 'blue',
+  });
+
+  oldLayer = e.target;
+
+  var feature = e.target.feature;
+  cityChart(feature);
+}
+
+function cityChart(feature) {
+  city = feature.properties.nome;
+
+  chart.options.title.text = city;
+  $.getJSON("http://127.0.0.1:8000/api/cities/" + city + "/1-1-1960/1-12-1960/",
+            function(data) {
+              dates = [];
+              values = [];
+
+              data.forEach((dt) => {
+                dates.push(dt.date);
+                values.push(dt.medTemp);
+              });
+
+              removeData(chart);
+              addData(chart, dates, values);
+              chart.update();
+
+            })
+}
 
 function onEachFeature(feature, layer) {
   var popupContent = "Nome:" + feature.properties.nome;
-  layer.bindPopup(popupContent);
-  layer.on("click", function(){refreshChart(feature.id);});
+  // layer.bindPopup(popupContent);
+  layer.on({
+    click: selectFeature,
+    mouseout: resetHighlight,
+    mouseover: highlightFeature,
+  })
 }
 
+
 var cities_layer = L.geoJson([], {
-  style: cities_layer_style,
+  style: style,
 
   onEachFeature: onEachFeature,
 })
