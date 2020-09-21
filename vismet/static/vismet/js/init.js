6d74ab6afc33a093a140deb89c8762a74fc83e74
Weiglas <weiglasribeiro@gmail.com>
2020-09-21 17:39:47 -0300

    Cria js para setar configurações iniciais de mapa/grafico/etc...

---
+++ vismet/static/vismet/js/init.js
@@ -0,0 +1,5 @@
+chart.options.scales.yAxes[0].scaleLabel.display = true;
+chart.options.scales.yAxes[0].scaleLabel.labelString = "Cº";
+chart.data.datasets[0].label = "Temperatura máxima";
+chart.options.legend.display = true;
+chart.update();
