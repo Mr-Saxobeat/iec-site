bad2888ddb20bfd9a5c8884a98c811ff09539010
Weiglas <weiglasribeiro@gmail.com>
2020-09-22 10:20:53 -0300

    Corrige "name" das urls
    - Corrige para o padrão de name para o singular da view.

--- vismet/urls.py
+++ vismet/urls.py
@@ -11,14 +11,12 @@ urlpatterns = [
          views.Api_XavierStations_Data,
          name='xavier_stations_timestamp'),
 
-    path('api/pixels/', views.Api_Pixel.as_view(), name='pixels'),
+    path('api/pixels/', views.Api_Pixel.as_view(), name='pixel'),
     path('api/pixels/<str:format>/<int:pk>/<int:start_day>-<int:start_month>-<int:start_year>/<int:final_day>-<int:final_month>-<int:final_year>/',
-         views.Api_Pixel_Data,
-         name='pixel_data'),
+         views.Api_Pixel_Data),
 
     path('api/cities/', views.Api_Cities.as_view(), name='cities'),
     path('api/cities/<str:name>/<int:start_day>-<int:start_month>-<int:start_year>/<int:final_day>-<int:final_month>-<int:final_year>/',
-         views.Api_Cities_Data,
-         name='cities_data'),
+         views.Api_Cities_Data),
 
 ]
