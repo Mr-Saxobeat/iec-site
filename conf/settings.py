0406bba273827d0923476a38b6ebaa8fc53795bb
Weiglas <weiglasribeiro@gmail.com>
2020-09-29 18:39:17 -0300

    Adiciona configuração de MIME type

--- conf/settings.py
+++ conf/settings.py
@@ -11,8 +11,12 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import mimetypes
 from decouple import config, Csv
 from dj_database_url import parse as db_url
+
+mimetypes.add_type("text/css", ".css", True)
+mimetypes.add_type("application/javascript", ".js", True)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
