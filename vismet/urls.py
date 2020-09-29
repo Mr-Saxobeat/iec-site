6ea4691b3bfc38b42d9fdf81e78d59def009fd88
Weiglas <weiglasribeiro@gmail.com>
2020-09-29 13:47:35 -0300

    Cria url para inmet stations e corrige argumento "omm_code"

--- vismet/urls.py
+++ vismet/urls.py
@@ -7,9 +7,14 @@ urlpatterns = [
     path('', views.VisMetView, name='vismet'),
 
     path('api/xavierstations/', views.Api_XavierStations.as_view(), name='xavier_stations'),
-    path('api/xavierstations/<str:format>/<int:omm_code>/<int:start_day>-<int:start_month>-<int:start_year>/<int:final_day>-<int:final_month>-<int:final_year>/',
+    path('api/xavierstations/<str:format>/<int:inmet_code>/<int:start_day>-<int:start_month>-<int:start_year>/<int:final_day>-<int:final_month>-<int:final_year>/',
          views.Api_XavierStations_Data,
          name='xavier_stations_timestamp'),
+
+    path('api/inmetstations/', views.Api_XavierStations.as_view(), name='inmet_stations'),
+    path('api/inmetstations/<str:format>/<int:inmet_code>/<int:start_day>-<int:start_month>-<int:start_year>/<int:final_day>-<int:final_month>-<int:final_year>/',
+         views.Api_INMET_Data,
+         name='inmet_stations_timestamp'),
 
     path('api/pixels/', views.Api_Pixel.as_view(), name='pixel'),
     path('api/pixels/<str:format>/<int:pk>/<int:start_day>-<int:start_month>-<int:start_year>/<int:final_day>-<int:final_month>-<int:final_year>/',
