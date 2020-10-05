760d601e1ffc770e2ec47fbb2aa2f86dfb3b60d9
Daniel <dddrasch@gmail.com>
2020-10-04 21:20:35 -0300

    Corrige a indicação de graus Celcius

--- vismet/static/vismet/js/init.js
+++ vismet/static/vismet/js/init.js
@@ -1,5 +1,5 @@
 chart.options.scales.yAxes[0].scaleLabel.display = true;
-chart.options.scales.yAxes[0].scaleLabel.labelString = "Cº";
+chart.options.scales.yAxes[0].scaleLabel.labelString = "ºC";
 chart.data.datasets[0].label = "Temperatura máxima";
 chart.options.legend.display = true;
 chart.update();
