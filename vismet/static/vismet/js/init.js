3f8540e13dc427d9aa198cef2e87dce351fbf41f
Weiglas <weiglasribeiro@gmail.com>
2020-10-08 16:21:42 -0300

    Cria funcs p mostrar o menu de acordo com a categoria de dados escolhida
    * init.js: 
      - Pega o json do servidor que contém as informações de categoria, 
    fontes e variáveis;
      - Inicializa o menu para categoria "observados";
      - Inicializa o mapa com a layer Xavier;
    * layers.js:
      - showCategoryData(): função que configura o menu de acordo com a 
    categoria de dados selecionada;
      - setSourceDataSelection(): função que configura o menu de seleção de 
    fonte de dados;

--- vismet/static/vismet/js/init.js
+++ vismet/static/vismet/js/init.js
@@ -11,3 +11,13 @@ chart.options.legend.display = true;
 chart.update();
 
 var url_stations = document.getElementById("url-stations").value;
+var url_data_options = document.getElementById("url-data-options").value;
+
+// Essa variável armazena as opções que poderão ser selecionadas
+// de acordo com a categoria, fonte, modelo.
+$.getJSON(url_data_options, function (data) {
+  json_data_options = JSON.parse(JSON.stringify(data));
+  loadJSONDataOptions(json_data_options);
+  showCategoryData("observados");
+  loadXavierLayer();
+});
--- vismet/static/vismet/js/layers.js
+++ vismet/static/vismet/js/layers.js
@@ -1,173 +1,73 @@
-map.addLayer(XavierStations_Layer);
+// Esta função irá mostrar o menu e as layers referentes à categoria de dados
+// a partir do botão clicado "observardos", "reanálise" ou "simulados".
+var json_data_options = [];
+var current_category = [];
 
-var divObservedData = document.getElementById("dados-observados");
-var divSimulatedData = document.getElementById("dados-simulados");
-var divByCity = document.getElementById("porMunicipio");
-var divByPixel = document.getElementById("porPixel");
-var selCamadaFuturos = document.getElementById("sel-camada-dados-futuros");
+var divObservedData = document.getElementById("div-observados");
+var divReanaliseData = document.getElementById("div-reanálise");
+var divSimulatedData = document.getElementById("div-simulados");
 
-divObservedData.style.display = "block";
-divSimulatedData.style.display = "none";
+function loadJSONDataOptions(data){
+  json_data_options = data;
+}
 
-
-document.getElementById("estacoesxavier").addEventListener("click", displayXavier);
-// document.getElementById("satelitereanalise").addEventListener("click", displaySatelite);
-document.getElementById("cenariosfuturos").addEventListener("click", displayFuturo);
-
-var XavierIsOn = true;
-var PixelIsOn = false;
-
-function displayXavier() {
-  if(XavierIsOn){
-    map.removeLayer(XavierStations_Layer);
-    XavierIsOn = false;
-  }
-  else{
-    map.addLayer(XavierStations_Layer);
-    XavierIsOn = true;
-
-    map.removeLayer(pixels_layer);
-    map.removeLayer(cities_layer);
-    PixelIsOn = false;
-
-    divObservedData.style.display = "block";
-    divSimulatedData.style.display = "none";
+function removeAllOptions(selectBox){
+  while(selectBox.options.length > 0){
+    selectBox.remove(0);
   }
 }
 
-function displayFuturo() {
-  if(PixelIsOn){
-    map.removeLayer(pixels_layer);
-    PixelIsOn = false;
-  }
-  else{
-    map.addLayer(pixels_layer);
-    PixelIsOn = true;
+function setVariableSelection(selectBox, source){
+  removeAllOptions(selectBox);
+  var i;
 
-    map.removeLayer(XavierStations_Layer);
-    removeData(chart);
-    chart.options.title.text = "";
-    chart.update();
-    XavierIsOn = false;
 
-    divObservedData.style.display = "none";
-    divSimulatedData.style.display = "block";
-    // document.getElementById("porMunicipio").style.display = "none";
-    // document.getElementById("porPixel").style.display = "block";
-    // document.getElementById("cenarios-futuros-layer").style.display = "block";
-    selCamadaFuturos.value = "pixel";
-    divByPixel.style.display = "block";
-    divByCity.style.display = "none";
-  }
 }
 
-selCamadaFuturos.addEventListener("change", function(){
-  var value = this.value;
-
-  if(value == "city"){
-    divByCity.style.display = "block";
-    divByPixel.style.display = "none";
-
-    map.removeLayer(pixels_layer);
-    map.addLayer(cities_layer);
-  }
-  else if(value == "pixel"){
-    divByCity.style.display = "none";
-    divByPixel.style.display = "block";
-
-    map.removeLayer(cities_layer);
-    map.addLayer(pixels_layer);
-  }
-})
-
-function displaySatelite() {
+function setSourceDataSelection(selectBox){
+  removeAllOptions(selectBox);
+  var newOpt;
+  current_category.sources.forEach(source => {
+    newOpt = new Option(source.name, source.name);
+    selectBox.add(newOpt, undefined);
+  });
 }
 
+function showCategoryData(div_last_name){
+  divObservedData.style.display = "none";
+  divReanaliseData.style.display = "none";
+  divSimulatedData.style.display = "none";
 
+  var selectedDiv = document.getElementById("div-" + div_last_name);
+  selectedDiv.style.display = "block";
+  var i = 0;
 
-var event_change = new Event('change');
-var sel_observados_fonte = document.getElementById("fonte-dados-observados");
-var sel_variavel = document.getElementById("station_variable");
-add_xavier_options();
-
-
-
-function clear_sel(){
-  var length = sel_variavel.options.length;
-  for (i = length-1; i >= 0; i--) {
-    sel_variavel.remove(i);
+  for(i = 0; i <= 10; i++){ // O valor de i <= foi arbitrário!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    if(json_data_options[i].category == div_last_name){
+      current_category = json_data_options[i];
+      break;
+    }
   }
+
+  var selectBox = selectedDiv.getElementsByClassName("sel-data-source")[0];
+  setSourceDataSelection(selectBox, div_last_name);
 }
 
-function add_xavier_options(){
-  var opt_maxTemp = document.createElement('option');
-    opt_maxTemp.appendChild(document.createTextNode('Temperatura Máxima'));
-    opt_maxTemp.value = "maxTemp";
-    sel_variavel.appendChild(opt_maxTemp);
+var btnObservedData = document.getElementById("btn-observados");
+var btnReanaliseData = document.getElementById("btn-reanálise");
+var btnSimulatedData = document.getElementById("btn-simulados");
 
-  var opt_minTemp = document.createElement('option');
-    opt_minTemp.appendChild(document.createTextNode('Temperatura Mínima'));
-    opt_minTemp.value = "minTemp";
-    sel_variavel.appendChild(opt_minTemp);
+btnObservedData.addEventListener("click", function() {
+  console.log("clicou " + btnObservedData.value);
+  showCategoryData(btnObservedData.value);
+});
 
-  var opt_evapo = document.createElement('option');
-    opt_evapo.appendChild(document.createTextNode('Evapotranspiração'));
-    opt_evapo.value = "evapo";
-    sel_variavel.appendChild(opt_evapo);
+btnReanaliseData.addEventListener("click", function() {
+  console.log("clicou " + btnReanaliseData.value);
+  showCategoryData(btnReanaliseData.value);
+});
 
-  var opt_relHum = document.createElement('option');
-    opt_relHum.appendChild(document.createTextNode('Umidade Relativa'));
-    opt_relHum.value = "relHum";
-    sel_variavel.appendChild(opt_relHum);
-
-  var opt_solarIns = document.createElement('option');
-    opt_solarIns.appendChild(document.createTextNode('Radiação Solar'));
-    opt_solarIns.value = "solarIns";
-    sel_variavel.appendChild(opt_solarIns);
-
-  var opt_windSpeed = document.createElement('option');
-    opt_windSpeed.appendChild(document.createTextNode('Velocidade do Vento'));
-    opt_windSpeed.value = "windSpeed";
-    sel_variavel.appendChild(opt_windSpeed);
-
-  sel_variavel.value = "maxTemp";
-  sel_variavel.dispatchEvent(event_change);
-}
-
-function add_inmet_options(){
-  var opt_maxTemp = document.createElement('option');
-    opt_maxTemp.appendChild(document.createTextNode('Temperatura Máxima'));
-    opt_maxTemp.value = "maxTemp";
-    sel_variavel.appendChild(opt_maxTemp);
-
-  var opt_minTemp = document.createElement('option');
-    opt_minTemp.appendChild(document.createTextNode('Temperatura Mínima'));
-    opt_minTemp.value = "minTemp";
-    sel_variavel.appendChild(opt_minTemp);
-
-  var opt_precip = document.createElement('option');
-    opt_precip.appendChild(document.createTextNode('Precipitação'));
-    opt_precip.value = "precip";
-    sel_variavel.appendChild(opt_precip);
-
-  var opt_relHum = document.createElement('option');
-    opt_relHum.appendChild(document.createTextNode('Umidade Relativa'));
-    opt_relHum.value = "relHum";
-    sel_variavel.appendChild(opt_relHum);
-
-  sel_variavel.value = "maxTemp";
-  sel_variavel.dispatchEvent(event_change);
-}
-
-
-sel_observados_fonte.addEventListener("change", function(){
-  var value = this.value;
-
-  if(value == "xavier"){
-    clear_sel();
-    add_xavier_options();
-  } else if(value == "inmet"){
-    clear_sel();
-    add_inmet_options();
-  }
-})
+btnSimulatedData.addEventListener("click", function() {
+  console.log("clicou " + btnSimulatedData.value);
+  showCategoryData(btnSimulatedData.value);
+});
