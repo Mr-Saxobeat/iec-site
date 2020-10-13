487172f479e51e76f0230f9e3ffa3c291facecbf
Weiglas <weiglasribeiro@gmail.com>
2020-10-13 17:48:47 -0300

    Remove configurações desnecessárias

--- conf/settings.py
+++ conf/settings.py
@@ -28,19 +28,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
-# Essa chave não está sendo usada no .env
-# SECRET_KEY = '-_bp9$l8$$ys$8j1ea28xgu8&6x-&4&@bwy)myp4hrvg2)0j3w'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default='False', cast=bool)
-# DEBUG = True
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
-# ALLOWED_HOSTS = ['iec.ufes.br', '127.0.0.1']
-
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -89,35 +82,12 @@ WSGI_APPLICATION = 'conf.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-DATABASES = {
-   'default': config('DATABASE_URL', cast=db_url)
-}
-
 # DATABASES = {
-#
-#     'default': {
-#
-# 	'ENGINE': 'django.contrib.gis.db.backends.postgis',
-#
-#         'NAME': 'iec',
-#
-#         'USER': 'postgres',
-#
-#         'PASSWORD': '',
-#
-#         'HOST': 'localhost',
-#
-#         'PORT': '',
-#
-#     }
-#
+#    'default': config('DATABASE_URL', cast=db_url)
 # }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -136,8 +106,6 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
-
-# LANGUAGE_CODE = 'en-us'
 LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'America/Sao_Paulo'
@@ -151,6 +119,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
