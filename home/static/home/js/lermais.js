ff3ee206ebb394a98cf1bce6f4d22488a1019c3f
Daniel <dddrasch@gmail.com>
2020-10-18 21:21:29 -0300

    Adiciona o 'Ler Mais', cuja programação deixa a desejar, muito embora funcione.

---
+++ home/static/home/js/lermais.js
@@ -0,0 +1,47 @@
+    function lerMais() {
+      var dots = document.getElementById("dots");
+      var moreText = document.getElementById("more");
+      var btnText = document.getElementById("myBtn");
+
+      if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Ler mais"; 
+        moreText.style.display = "none";
+      } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Ler menos"; 
+        moreText.style.display = "inline";
+      }
+    }
+
+        function lerMais2() {
+      var dots = document.getElementById("dots2");
+      var moreText = document.getElementById("more2");
+      var btnText = document.getElementById("myBtn2");
+
+      if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Ler mais"; 
+        moreText.style.display = "none";
+      } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Ler menos"; 
+        moreText.style.display = "inline";
+      }
+    }
+
+        function lerMais3() {
+      var dots = document.getElementById("dots3");
+      var moreText = document.getElementById("more3");
+      var btnText = document.getElementById("myBtn3");
+
+      if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Ler mais"; 
+        moreText.style.display = "none";
+      } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Ler menos"; 
+        moreText.style.display = "inline";
+      }
+    }
