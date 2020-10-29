5f0f5d1824c1204afefe5ed4202eb94a43149264
Weiglas <weiglasribeiro@gmail.com>
2020-10-29 02:29:10 -0300

    Remove importação de cities

--- vismet/scripts/setstations.py
+++ vismet/scripts/setstations.py
@@ -3,7 +3,6 @@ import vismet.scripts.inmet as inmet
 import vismet.scripts.ana as ana
 import vismet.scripts.categories as categories
 import vismet.scripts.pixel as pixel
-import vismet.scripts.city as city
 
 def run():
     categories.LoadCategories()
@@ -12,6 +11,5 @@ def run():
     ana.LoadANAStations()
     ana.LoadANAStations2()
     pixel.LoadPixels()
-    city.LoadCities()
 
     print("\n\nTodas os elementos foram carregados.")
