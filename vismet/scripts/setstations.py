cfc44e0a84bacb3343352d726f4ed8003fbd4ccc
Weiglas <weiglasribeiro@gmail.com>
2020-10-28 23:56:50 -0300

    Chama função para carregar layer City

--- vismet/scripts/setstations.py
+++ vismet/scripts/setstations.py
@@ -3,6 +3,7 @@ import vismet.scripts.inmet as inmet
 import vismet.scripts.ana as ana
 import vismet.scripts.categories as categories
 import vismet.scripts.pixel as pixel
+import vismet.scripts.city as city
 
 def run():
     categories.LoadCategories()
@@ -11,5 +12,6 @@ def run():
     ana.LoadANAStations()
     ana.LoadANAStations2()
     pixel.LoadPixels()
+    city.LoadCities()
 
     print("\n\nTodas os elementos foram carregados.")
