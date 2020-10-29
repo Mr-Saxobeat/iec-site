bd35ae6982501a50ef08321bd91669d6184ec3f7
Weiglas <weiglasribeiro@gmail.com>
2020-10-29 10:27:38 -0300

    Usa a função de criar gráfico

--- vismet/static/vismet/js/init.js
+++ vismet/static/vismet/js/init.js
@@ -1,22 +1,16 @@
-var dataSet_Start = {
-  label: "Temperatura Máxima",
-  data: [],
-}
-
-chart.data.datasets.push(dataSet_Start);
-
-chart.options.scales.yAxes[0].scaleLabel.display = true;
-chart.options.scales.yAxes[0].scaleLabel.labelString = "ºC";
-chart.options.legend.display = true;
-chart.update();
 
 var url_stations = document.getElementById("url-stations").value;
 var url_data_options = document.getElementById("url-data-options").value;
 
-
 // Essa variável armazena as opções que poderão ser selecionadas
 // de acordo com a categoria, fonte, modelo.
 $.getJSON(url_data_options, function (data) {
+  createNewChart('line');
+  chart.options.scales.yAxes[0].scaleLabel.display = true;
+  chart.options.scales.yAxes[0].scaleLabel.labelString = "ºC";
+  chart.options.legend.display = true;
+  chart.update();
+
   json_data_options = JSON.parse(JSON.stringify(data));
   loadXavierLayer();
   loadANALayer();
@@ -26,6 +20,8 @@ $.getJSON(url_data_options, function (data) {
   showCategoryData("observados");
   map.addLayer(layers_dic["ana"]);
 });
+
+
 
 var btn_submit = document.getElementById("btn_submit");
 var btn_download = document.getElementById("btn_download");
