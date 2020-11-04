9b896b1b4cafdbddcac0e37db33185582c76e612
Weiglas <weiglasribeiro@gmail.com>
2020-11-04 16:24:19 -0300

    Configura corretamente urls da home page e do vismet

--- conf/urls.py
+++ conf/urls.py
@@ -20,8 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('vismet.urls')),
-    path('home/', include('home.urls')),
+    path('plataforma/', include('vismet.urls')),
+    path('', include('home.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
