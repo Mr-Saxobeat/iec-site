1804802a61467039ba15b07beea73e79422d94a5
Weiglas <weiglasribeiro@gmail.com>
2020-11-05 11:29:28 -0300

    Configura admin model para ElementCategory

--- vismet/admin.py
+++ vismet/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from vismet.models import ElementCategory, ElementVariable, DataModel, ElementSource, Station
 
-admin.site.register(ElementCategory)
+class ElementCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'display_name', 'desc')
+    fields = ('display_name', 'desc')
+
+admin.site.register(ElementCategory, ElementCategoryAdmin)
 admin.site.register(ElementVariable)
 admin.site.register(ElementSource)
 admin.site.register(Station)
