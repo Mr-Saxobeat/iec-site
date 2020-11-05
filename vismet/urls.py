df6d7d10cb3afee3da52804fc5c51308b4c959cc
Weiglas <weiglasribeiro@gmail.com>
2020-11-05 08:50:46 -0300

    Corrige url para a view GeoJSONLayerView de estações

--- vismet/urls.py
+++ vismet/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
 
     path('api/opcoes/', views.Api_Data_Options, name='data_options'),
     path('api/estacoes/', views.Api_Stations, name='api_stations'),
-    path('api/estacoes/<str:format>/<str:source>/', views.Api_Stations_Source, name='api_stations_source'),
+    path('api/estacoes/<format>/<source>/<type>/', views.Api_Stations_Source.as_view(), name='api_stations_source'),
     path('api/estacoes/<str:format>/<str:source>/<str:code>/<int:start_day>-<int:start_month>-<int:start_year>/<int:final_day>-<int:final_month>-<int:final_year>/',
          views.Api_Stations_Data,
          name='api_station_data'),
