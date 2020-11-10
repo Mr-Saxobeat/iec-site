de8350097c7f027fc0c4face0de91dc530a5c2b1
Weiglas <weiglasribeiro@gmail.com>
2020-11-10 11:27:47 -0300

    Corrige registro dos models ElementCategory e SubSource

--- vismet/admin.py
+++ vismet/admin.py
@@ -3,11 +3,11 @@ from vismet.models import ElementCategory, ElementVariable, DataModel, ElementSo
 
 class ElementCategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'display_name', 'desc')
-    fields = ('display_name', 'desc')
+    fields = ('display_name', 'desc', 'subsource')
 
 class SubSourceAdmin(admin.ModelAdmin):
     list_display = ('display_name', 'desc', 'time_interval')
-    fields = ('display_name', 'desc', 'time_interval')
+    fields = ('display_name', 'desc', 'time_interval', 'related_source')
 
 admin.site.register(ElementCategory, ElementCategoryAdmin)
 admin.site.register(SubSource, SubSourceAdmin)
