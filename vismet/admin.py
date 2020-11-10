32472e78cf00e3e854e1378bf444c20f97e20422
Weiglas <weiglasribeiro@gmail.com>
2020-11-10 11:08:07 -0300

    Registra o model 'SubSource' ao admin

--- vismet/admin.py
+++ vismet/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
-from vismet.models import ElementCategory, ElementVariable, DataModel, ElementSource, Station
+from vismet.models import ElementCategory, ElementVariable, DataModel, ElementSource, SubSource, Station
 
 class ElementCategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'display_name', 'desc')
     fields = ('display_name', 'desc')
 
+class SubSourceAdmin(admin.ModelAdmin):
+    list_display = ('display_name', 'desc', 'time_interval')
+    fields = ('display_name', 'desc', 'time_interval')
+
 admin.site.register(ElementCategory, ElementCategoryAdmin)
+admin.site.register(SubSource, SubSourceAdmin)
 admin.site.register(ElementVariable)
 admin.site.register(ElementSource)
 admin.site.register(Station)
