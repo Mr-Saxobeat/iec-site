42074151f8ce559bf4d6bbe537efaeec9d2584c7
Weiglas <weiglasribeiro@gmail.com>
2020-11-11 10:11:57 -0300

    Utiliza OSMGeoAdmin para o admin de Station

--- vismet/admin.py
+++ vismet/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.gis import admin as geoadmin
 from vismet.models import Station, ElementCategory, ElementVariable, DataModel, ElementSource, SubSource, SubSourceDetail
 
 class ElementCategoryAdmin(admin.ModelAdmin):
@@ -14,9 +15,23 @@ class SubSourceDetailAdmin(admin.ModelAdmin):
 class ElementSourceAdmin(admin.ModelAdmin):
     list_display = ('display_name',)
 
+class StationAdmin(geoadmin.OSMGeoAdmin):
+    fields = (
+                'source',
+                'omm_code',
+                'inmet_code',
+                'state',
+                'city',
+                'type',
+                'altitude',
+                'startDate',
+                'finalDate',
+                'status',
+                )
+
 admin.site.register(ElementCategory, ElementCategoryAdmin)
 admin.site.register(SubSource, SubSourceAdmin)
 admin.site.register(SubSourceDetail, SubSourceDetailAdmin)
 admin.site.register(ElementVariable)
 admin.site.register(ElementSource, ElementSourceAdmin)
-admin.site.register(Station)
+admin.site.register(Station, StationAdmin)
