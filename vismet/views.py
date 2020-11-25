ae9a8896888475375bf12eff424f1f30103db5a9
Weiglas <weiglasribeiro@gmail.com>
2020-11-25 11:22:00 -0300

    Adiciona método 'order_by('date')'

--- vismet/views.py
+++ vismet/views.py
@@ -136,7 +136,7 @@ def Api_Pixel_Data(request, format, pk, start_day, start_month, start_year, fina
 
     try:
         pixel = Pixel.objects.get(pk=pk)
-        data = pixel.pixel_data.filter(date__gte=startDate, date__lte=finalDate)
+        data = pixel.pixel_data.filter(date__gte=startDate, date__lte=finalDate).order_by('date')
     except Pixel.DoesNotExist:
         return JsonResponse({'mensagem': 'Pixel não encontrado.'}, status=404)
     except PixelData.DoesNotExist:
