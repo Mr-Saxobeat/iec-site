2a731387ba3ccd1aa134ac28f992bc47b686e904
Weiglas <weiglasribeiro@gmail.com>
2020-12-08 22:11:47 -0300

    Corrige script para pegar estacoes inmet

--- vismet/scripts/inmet.py
+++ vismet/scripts/inmet.py
@@ -2,7 +2,7 @@ import os
 import csv
 import datetime
 from vismet.models import ElementCategory, ElementSource, Station
-from django.contrib.gis.geos import Point
+from django.contrib.gis.geos import Point, LinearRing, Polygon
 import requests
 
 # Esse script pega as estações xavier da API do INMET
@@ -20,23 +20,80 @@ def LoadINMETStations(csv_path=os.path.join(os.getcwd(), 'vismet', 'scripts', 'd
     automatic_stations_response = requests.get("https://apitempo.inmet.gov.br/estacoes/T").json()
     manual_stations_response = requests.get("https://apitempo.inmet.gov.br/estacoes/M").json()
 
-    list_inmet_codes = []
+    # list_inmet_codes = []
 
-    # Lê os códigos INMET das estações necessárias.
-    with open(csv_path, 'r') as file:
-        reader = csv.reader(file)
-
-        for row in reader:
-            list_inmet_codes.append(row[0])
-    file.close()
+    # # Lê os códigos INMET das estações necessárias.
+    # with open(csv_path, 'r') as file:
+    #     reader = csv.reader(file)
+    #
+    #     for row in reader:
+    #         list_inmet_codes.append(row[0])
+    # file.close()
 
     startDate = None
     finalDate = None
     created_station = None
 
-    # Cria models das estações automáticas
+    minLat = -20
+    minLon = -40
+    maxLat = -15
+    maxLon = -39
+
+    pt1 = (minLon, minLat)
+    pt2 = (minLon, maxLat)
+    pt3 = (maxLon, maxLat)
+    pt4 = (maxLon, minLat)
+
+    polygon_es = Polygon(LinearRing(pt1, pt2, pt3, pt4, pt1))
+
     for station in automatic_stations_response:
-        if station["CD_ESTACAO"] in list_inmet_codes:
+        station_lat = float(station["VL_LATITUDE"])
+        station_lon = float(station["VL_LONGITUDE"])
+        station_point = Point(station_lon, station_lat, None, 4326)
+
+        if (station_lat >= minLat and station_lon >= minLon and
+            station_lat <= maxLat and station_lon <= maxLon):
+    # # Cria models das estações automáticas
+    # for station in automatic_stations_response:
+    #     if station["CD_ESTACAO"] in list_inmet_codes:
+            startDate = station["DT_INICIO_OPERACAO"]
+            finalDate = station["DT_FIM_OPERACAO"]
+
+            if startDate:
+                startDate = datetime.datetime.strptime(startDate[:10], "%Y-%m-%d")
+            else:
+                startDate = None
+
+            if finalDate:
+                finalDate = datetime.datetime.strptime(finalDate[:10], "%Y-%m-%d")
+            else:
+                finalDate = None
+
+            # if station["CD_ESTACAO"] in list_inmet_codes:
+
+            newObj, created = Station.objects.get_or_create(
+                source = source,
+                inmet_code = station["CD_ESTACAO"],
+                state = station["SG_ESTADO"],
+                city = station["DC_NOME"],
+                type = station["TP_ESTACAO"],
+                altitude = station["VL_ALTITUDE"],
+                startDate = startDate,
+                finalDate = finalDate,
+                status = station["CD_SITUACAO"],
+                geom = point,
+            )
+
+            if created:
+                print(newObj)
+
+    # Cria models das estações convecionais
+    for station in manual_stations_response:
+        station_lat = float(station["VL_LATITUDE"])
+        station_lon = float(station["VL_LONGITUDE"])
+
+        if (station_lat >= minLat and station_lon >= minLon and
+            station_lat <= maxLat and station_lon <= maxLon):
 
             startDate = station["DT_INICIO_OPERACAO"]
             finalDate = station["DT_FIM_OPERACAO"]
@@ -51,55 +108,23 @@ def LoadINMETStations(csv_path=os.path.join(os.getcwd(), 'vismet', 'scripts', 'd
             else:
                 finalDate = None
 
-            if station["CD_ESTACAO"] in list_inmet_codes:
-                point = Point(station["VL_LONGITUDE"], station["VL_LATITUDE"], None, 4326)
-                newObj, created = Station.objects.get_or_create(
-                    source = source,
-                    inmet_code = station["CD_ESTACAO"],
-                    state = station["SG_ESTADO"],
-                    city = station["DC_NOME"],
-                    type = station["TP_ESTACAO"],
-                    altitude = station["VL_ALTITUDE"],
-                    startDate = startDate,
-                    finalDate = finalDate,
-                    status = station["CD_SITUACAO"],
-                    geom = point,
-                )
-
-            print(newObj)
-
-    # Cria models das estações convecionais
-    for station in manual_stations_response:
-
-        startDate = station["DT_INICIO_OPERACAO"]
-        finalDate = station["DT_FIM_OPERACAO"]
-
-        if startDate:
-            startDate = datetime.datetime.strptime(startDate[:10], "%Y-%m-%d")
-        else:
-            startDate = None
-
-        if finalDate:
-            finalDate = datetime.datetime.strptime(finalDate[:10], "%Y-%m-%d")
-        else:
-            finalDate = None
-
-        if station["CD_ESTACAO"] in list_inmet_codes:
+            # if station["CD_ESTACAO"] in list_inmet_codes:
+            point = Point(station_lon, station_lat, None, 4326)
             newObj, created = Station.objects.get_or_create(
                 source = source,
                 inmet_code = station["CD_ESTACAO"],
                 state = station["SG_ESTADO"],
                 city = station["DC_NOME"],
                 type = station["TP_ESTACAO"],
-                latitude = station["VL_LATITUDE"],
-                longitude = station["VL_LONGITUDE"],
                 altitude = station["VL_ALTITUDE"],
                 startDate = startDate,
                 finalDate = finalDate,
                 status = station["CD_SITUACAO"],
+                geom = point,
             )
 
-        print(newObj)
+            if created:
+                print(newObj)
 
     return print("Estações INMET foram carregadas.")
 
