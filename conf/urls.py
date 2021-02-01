08183c1c0cd70f8f31b18a6ad658523ddedcd958
Weiglas <weiglasribeiro@gmail.com>
2021-02-01 14:40:56 -0300

    Adiciona url para api2

--- conf/urls.py
+++ conf/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('plataforma/', include('vismet.urls')),
     path('', include('home.urls')),
+    path('api2/', include('vismet.api.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
