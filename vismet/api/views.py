cb159ff59de112de5e19984f61db2f29dc3c9f48
Weiglas <weiglasribeiro@gmail.com>
2021-02-02 18:30:08 -0300

    Adiciona verificação de dado repetido

--- vismet/api/views.py
+++ vismet/api/views.py
@@ -1,6 +1,8 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 from vismet.models import City, DataModel, CityData
 from vismet.api.serializers import CityDataSerializer
+import datetime
 
 
 class CityDataAPIView(generics.ListCreateAPIView):
@@ -11,11 +13,30 @@ class CityDataAPIView(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         city_fid = self.request.data['city_fid']
         data_model_name = self.request.data['data_model']
-        
-        city = City.objects.get(fid=city_fid)
-        data_model = DataModel.objects.get(name=data_model_name)
+        date = self.request.data['date']
 
-        serializer.save(city=city, data_model=data_model)
+        try:
+            date_aux = datetime.datetime.strptime(date, '%Y-%m-%d').date()
+            citydata = CityData.objects.get(
+                            city__fid=city_fid, 
+                            data_model__name=data_model_name, 
+                            date=date_aux
+                        )
+            
+            if citydata != None:
+                return Response(
+                            data={
+                                'mensagem': 'O dado para a data, modelo e cidade fornecidos já existe.'
+                                }, 
+                            status=status.HTTP_401_UNAUTHORIZED
+                            )
+
+        except CityData.DoesNotExist:
+            city = City.objects.get(fid=city_fid)
+            print(data_model_name)
+            data_model = DataModel.objects.get(name__icontains=data_model_name)
+
+            serializer.save(city=city, data_model=data_model)
         
 
     
