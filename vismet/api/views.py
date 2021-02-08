050e529bb3bf76d89d85ddd8cbcf7ffd79ddb4d3
Weiglas <weiglasribeiro@gmail.com>
2021-02-08 14:19:14 -0300

    Corrige view

--- vismet/api/views.py
+++ vismet/api/views.py
@@ -8,29 +8,6 @@ import datetime
 class CityDataAPIView(generics.ListCreateAPIView):
     queryset = CityData.objects.all()
     serializer_class = CityDataSerializer
-
-    def post(self, request, *args, **kwargs):
-        data_model = request.data['data_model']
-        date = request.data['date']
-        fid = request.data['fid']
-
-        try:
-            date_aux = datetime.datetime.strptime(date, '%Y-%m-%d').date()
-            citydata = CityData.objects.get(
-                            city__fid=city_fid, 
-                            data_model__name=data_model_name, 
-                            date=date_aux
-                        )
-            
-            if citydata != None:
-                return Response(
-                            data={
-                                'mensagem': 'O dado para a data, modelo e cidade fornecidos já existe.'
-                                }, 
-                            status=status.HTTP_401_UNAUTHORIZED
-                            )
-        
-        return self.create(request, *args, **kwargs)
     
     def perform_create(self, serializer):
         city_fid = self.request.data['city_fid']
