a9f1b2aa687e67f1a4a792e04bb8026ff7c6da71
Weiglas <weiglasribeiro@gmail.com>
2021-02-08 19:40:01 -0300

    Adiciona condição pra criar objeto novo

--- vismet/api/views.py
+++ vismet/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics, status
 from rest_framework.response import Response
-from vismet.models import City, DataModel, CityData
-from vismet.api.serializers import CityDataSerializer
+from vismet.models import City, DataModel, CityData, Pixel, PixelData
+from vismet.api.serializers import CityDataSerializer, PixelDataSerializer
 import datetime
 
 
@@ -12,16 +12,40 @@ class CityDataAPIView(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         city_fid = self.request.data['city_fid']
         data_model_name = self.request.data['data_model']
-        date = self.request.data['date']
+        date = datetime.datetime.strptime(self.request.data['date'], '%Y-%m-%d')
+
+        pd = CityData.objects.filter(
+                city__fid=city_fid, 
+                data_model__name__contains=data_model_name,
+                date=date
+                )
+        
+        if pd == None:
+            print('bora criar')
+            city = City.objects.get(fid=city_fid)
+            data_model = DataModel.objects.get(name__icontains=data_model_name)
+            serializer.save(city=city, data_model=data_model)
+
+class PixelDataAPIView(generics.ListCreateAPIView):
+    queryset = PixelData.objects.all()
+    serializer_class = PixelDataSerializer
+
+    def perform_create(self, serializer):
+        lat = self.request.data['latitude']
+        lon = self.request.data['longitude']
+        data_model_name = self.request.data['data_model_name']
+
+        pixel = Pixel.objects.get(latitude=lat, longitude=lon)
+        data_model = DataModel.objects.get(name__icontains=data_model_name)
+
+        serializer.save(
+            pixel=pixel,
+            data_model=data_model
+        )
+
 
         
 
-
-        city = City.objects.get(fid=city_fid)
-        print(data_model_name)
-        data_model = DataModel.objects.get(name__icontains=data_model_name)
-
-        serializer.save(city=city, data_model=data_model)
         
 
     
