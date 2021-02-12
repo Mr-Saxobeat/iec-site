513fe2dd017c507884d92ac477be034aaea46260
Weiglas <weiglasribeiro@gmail.com>
2021-02-12 18:01:07 -0300

    Corrige nome da view

--- vismet/api/urls.py
+++ vismet/api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from vismet.api.views import CityDataAPIView, PixelDataListCreateAPIView
+from vismet.api.views import CityDataAPIView, PixelDataRetrieveCreateAPIView
 
 
 urlpatterns = [
     path('citydata/', CityDataAPIView.as_view(), name='city-data-list'),
-    path('pixeldata/', PixelDataListCreateAPIView.as_view(), name='pixel-data-list'),
+    path('pixeldata/', PixelDataRetrieveCreateAPIView.as_view(), name='pixel-data-list'),
 ]
