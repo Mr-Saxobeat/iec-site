e72da18725d0b831a00feedb13a50f8635f86bc0
daniel <dddrasch@gmail.com>
2021-02-24 23:00:03 -0300

    Exibe o gráfico em uma nova aba ao clicar em 'Salvar Gráfico'

--- vismet/static/vismet/js/botoes.js
+++ vismet/static/vismet/js/botoes.js
@@ -45,7 +45,7 @@ function screenshot(){
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;  
   html2canvas(document.getElementById("chart")).then(canvas=>{
-    var image = canvas.toDataURL("image/png").replace("image/png","image/octet-stream");  
+    var image = canvas.toDataURL("image/png");  
     window.location.href=image
   });
 }
