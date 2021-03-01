7059b4f267049e5d9c77156050faa119cd4ef3b4
Weiglas <weiglasribeiro@gmail.com>
2021-03-01 17:09:16 -0300

    Renomeia script

--- vismet/scripts/era5_monthly.py
+++ vismet/scripts/era5_monthly.py
@@ -7,21 +7,13 @@ import xarray as xr
 
 from vismet.models import DataModel, Pixel, PixelData
 
-
-def run(var='2t'):
-    log_file_dir = os.path.join(os.getcwd(), 'pixel025_log.txt')
-    log_file = open(log_file_dir, 'w')
-    fn = '/home/weiglas/Documents/iec/dados/reanalise/era5/descompressed/1month_mean_Global_ea_' + var + '_201501_v02.grib'
-    ds = xr.open_dataset(fn, engine='cfgrib')
-    return ds
-
 def addMonth(date):
     if date.month == 12:
         return datetime.datetime(date.year + 1, 1, 1)
     else:
         return datetime.datetime(date.year, date.month + 1, date.day)
 
-def run2(path = '/home/weiglas/Documents/iec/dados/reanalise/era5/descompressed',
+def run(path = '/home/weiglas/Documents/iec/dados/reanalise/era5/monthly/descompressed',
          date_str='1979-01-01',
          finalDate_str='2021-01-01',
          url='https://iec.ufes.br/plataforma/api/pixeldata/'):
@@ -58,8 +50,8 @@ def run2(path = '/home/weiglas/Documents/iec/dados/reanalise/era5/descompressed'
         strDate = '_' + date.strftime('%Y%m')
 
         ds_2t = xr.open_dataset(os.path.join(path, file_init + '2t' + strDate + file_final), engine='cfgrib')
-        ds_ci = xr.open_dataset(os.path.join(path, file_init + 'ci' + strDate + file_final), engine='cfgrib')
-        ds_swvl1 = xr.open_dataset(os.path.join(path, file_init + 'swvl1' + strDate + file_final), engine='cfgrib')
+        # ds_ci = xr.open_dataset(os.path.join(path, file_init + 'ci' + strDate + file_final), engine='cfgrib')
+        # ds_swvl1 = xr.open_dataset(os.path.join(path, file_init + 'swvl1' + strDate + file_final), engine='cfgrib')
         ds_r2 = xr.open_dataset(os.path.join(path, file_init + 'r2' + strDate + file_final), engine='cfgrib')
         ds_tp = xr.open_dataset(os.path.join(path, file_init + 'tp' + strDate + file_final), engine='cfgrib')
 
@@ -72,14 +64,14 @@ def run2(path = '/home/weiglas/Documents/iec/dados/reanalise/era5/descompressed'
             longitudes = ((ds_2t['longitude'] + 180) % 360 - 180).data.tolist()
 
         xr_2t = ds_2t['t2m']
-        xr_ci = ds_ci['siconc']
-        xr_swvl1 = ds_swvl1['swvl1']
+        # xr_ci = ds_ci['siconc']
+        # xr_swvl1 = ds_swvl1['swvl1']
         xr_r2 = ds_r2['r']
         xr_tp = ds_tp['tp']
 
         ds_2t.close()
-        ds_ci.close()
-        ds_swvl1.close()
+        # ds_ci.close()
+        # ds_swvl1.close()
         ds_r2.close()
         ds_tp.close()
 
@@ -102,27 +94,28 @@ def run2(path = '/home/weiglas/Documents/iec/dados/reanalise/era5/descompressed'
                     continue
 
             val_2t = xr_2t[i_lat, i_lon].data.tolist()
-            val_ci = xr_ci[i_lat, i_lon].data.tolist()
-            val_swvl1 = xr_swvl1[i_lat, i_lon].data.tolist()
+            # val_ci = xr_ci[i_lat, i_lon].data.tolist()
+            # val_swvl1 = xr_swvl1[i_lat, i_lon].data.tolist()
             val_r2 = xr_r2[i_lat, i_lon].data.tolist()
             val_tp = xr_tp[i_lat, i_lon].data.tolist()
 
             val_2t = None if math.isnan(val_2t) else val_2t
-            val_ci = None if math.isnan(val_ci) else val_ci
-            val_swvl1 = None if math.isnan(val_swvl1) else val_swvl1
+            # val_ci = None if math.isnan(val_ci) else val_ci
+            # val_swvl1 = None if math.isnan(val_swvl1) else val_swvl1
             val_r2 = None if math.isnan(val_r2) else val_r2
             val_tp = None if math.isnan(val_tp) else val_tp
 
             data = {
+                'date': date.strftime('%Y-%m-%d'),
                 'latitude': p.latitude,
                 'longitude': p.longitude,
                 'resolution': 0.25,
                 'data_model_name': 'era5',
-                'evapo': val_2t,
-                'maxTemp': val_ci,
-                'minTemp': val_swvl1,
-                'precip': val_r2,
-                'ocis': val_tp,
+                'tp2m': val_2t,
+                'relHum': val_r2,
+                'precip': val_tp,
+                # 'maxTemp': val_ci,
+                # 'minTemp': val_swvl1,
             }
 
             response = requests.post(url, data=data, verify=False)
@@ -131,6 +124,7 @@ def run2(path = '/home/weiglas/Documents/iec/dados/reanalise/era5/descompressed'
                 print(str(response.status_code) + ': ' + str(p.latitude) + ', ' + str(p.longitude) + ' - ' + date.strftime('%m/%Y'))
             else:
                 print(response.content)
+                log_file.write(p.latitude + ', ' + p.longitude + ' -- ' + )
         
         date = addMonth(date)
     
