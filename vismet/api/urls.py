eb5fa5eefb4be091a0791fedfe81dc51fe686ac9
Weiglas <weiglasribeiro@gmail.com>
2021-03-03 13:14:05 -0300

    Cria url para dados pixel em json list

--- vismet/api/urls.py
+++ vismet/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from vismet.api.views import CityDataAPIView, PixelDataRetrieveCreateAPIView
+from vismet.api.views import CityDataAPIView, PixelDataRetrieveCreateAPIView, PixelDataCreateFromListAPIView
 
 
 urlpatterns = [
     path('citydata/', CityDataAPIView.as_view(), name='city-data-list'),
     path('pixeldata/', PixelDataRetrieveCreateAPIView.as_view(), name='pixel-data-list'),
+    path('pixeldatalist/', PixelDataCreateFromListAPIView.as_view(), name='pixel-data-create-list'),
 ]
