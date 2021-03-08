38ec97e68a0f2fef894db624f4945c1cdfe517ab
daniel <dddrasch@gmail.com>
2021-03-08 12:03:04 -0300

    Adiciona um calendário para cada fonte, sendo mensais os de Cenários e Reanálises

--- vismet/static/vismet/js/datepicker.js
+++ vismet/static/vismet/js/datepicker.js
@@ -1,14 +1,66 @@
-$(".dateinput").datepicker({
- showOn: 'focus',
- dateFormat: 'dd/mm/yy',
- dayNames: ['Domingo','Segunda','Terça','Quarta','Quinta','Sexta','Sábado'],
- dayNamesMin: ['D','S','T','Q','Q','S','S','D'],
- dayNamesShort: ['Dom','Seg','Ter','Qua','Qui','Sex','Sáb','Dom'],
- monthNames: ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
- monthNamesShort: ['Jan','Fev','Mar','Abr','Mai','Jun','Jul','Ago','Set','Out','Nov','Dez'],
- nextText: 'Próximo',
- prevText: 'Anterior',
- showOtherMonths: true,
- changeYear: true,
- changeMonth: true,
+$(function() {
+     $('.month-picker').datepicker(
+                    {
+                        dateFormat: "mm/yy",
+                        changeMonth: true,
+                        changeYear: true,
+                        showButtonPanel: true,
+                        todayBtn: false,
+                        closeText: 'Selecionar',
+                        onClose: function(dateText, inst) {
+
+
+                            function isDonePressed(){
+                                return ($('#ui-datepicker-div').html().indexOf('ui-datepicker-close ui-state-default ui-priority-primary ui-corner-all ui-state-hover') > -1);
+                            }
+
+                            if (isDonePressed()){
+                                var month = $("#ui-datepicker-div .ui-datepicker-month :selected").val();
+                                var year = $("#ui-datepicker-div .ui-datepicker-year :selected").val();
+                                $(this).datepicker('setDate', new Date(year, month, 1)).trigger('change');
+                                
+                                 $('.month-picker').focusout()//Added to remove focus from datepicker input box on selecting date
+                            }
+                        },
+                        beforeShow : function(input, inst) {
+                            setTimeout(function(){
+                            $(input).datepicker("widget").find(".ui-datepicker-current").hide();
+                            })
+                            inst.dpDiv.addClass('month_year_datepicker')
+
+                            if ((datestr = $(this).val()).length > 0) {
+                                year = datestr.substring(datestr.length-4, datestr.length);
+                                month = datestr.substring(0, 2);
+                                $(this).datepicker('option', 'defaultDate', new Date(year, month-1, 1));
+                                $(this).datepicker('setDate', new Date(year, month-1, 1));
+                                $(".ui-datepicker-calendar").hide();
+                            }
+                        }
+                    })
+});
+
+$(function() {
+    $(".dateinput").datepicker({
+    showOn: 'focus',
+    dateFormat: 'dd/mm/yy',
+    dayNames: ['Domingo','Segunda','Terça','Quarta','Quinta','Sexta','Sábado'],
+    dayNamesMin: ['D','S','T','Q','Q','S','S','D'],
+    dayNamesShort: ['Dom','Seg','Ter','Qua','Qui','Sex','Sáb','Dom'],
+    monthNames: ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
+    monthNamesShort: ['Jan','Fev','Mar','Abr','Mai','Jun','Jul','Ago','Set','Out','Nov','Dez'],
+    nextText: 'Próximo',
+    prevText: 'Anterior',
+    showOtherMonths: true,
+    showButtonPanel: true,
+    closeText: 'Selecionar',
+    currentText: 'Hoje',
+    todayBtn: false,
+    changeYear: true,
+    changeMonth: true,
+    beforeShow : function(input, inst) {
+        setTimeout(function(){
+        $(input).datepicker("widget").find(".ui-datepicker-current").hide();
+        })
+    }
+    });
 });
