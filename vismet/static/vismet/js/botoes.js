5fa8378bcc91076e44d1c11582e42f8a96f2e5d8
daniel <dddrasch@gmail.com>
2021-03-11 14:46:45 -0300

    Adiciona radio buttons para diário e mensal, com o diário indisponível nas Reanálises e nos Cenários

--- vismet/static/vismet/js/botoes.js
+++ vismet/static/vismet/js/botoes.js
@@ -1,5 +1,6 @@
 $("#btn_submit, #btn_download").prop('disabled', true);
 
+
 var botoes = botoes;
 
 var toValidate = $('#startDate, #finalDate'),
@@ -32,6 +33,21 @@ $(document).ready(function(){
   });
 });
 
+$(document).ready(function(){
+  $("getData").on("click",function(){
+    $.ajax({
+      type:"GET",
+      url:"https://apitempo.inmet.gov.br/estacoes",
+      beforeSend: function(){
+        $("loader").show();
+      },
+      complete: function(){
+        $("loader").hide();
+      },
+    });
+  });
+});
+
 function screenshot(){
     html2canvas(document.getElementById("chartdownload")).addEventListener('click', function(){canvas=>{
       var url_base64jp = document.getElementById("chart").toDataURL("image/png");
@@ -55,3 +71,92 @@ $("#btn_submit").click(function() {
   return false;
 });
 
+
+
+$(document).ready(function(){
+    $('.frequency').on('change', function() {
+      if ( this.value == '1')
+      {
+        $(".day").show();
+        $(".month").hide();
+      }
+      else
+      {
+        $(".day").hide();
+        $(".month").show();
+      }
+    });
+});
+
+$(document).ready(function() {
+  const monthNames = ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'];
+  let qntYears = 4;
+  let selectYear = $("#year");
+  let selectMonth = $("#month");
+  let selectDay = $("#day");
+  let currentYear = new Date().getFullYear();
+
+  for (var y = 0; y < qntYears; y++) {
+    let date = new Date(currentYear);
+    let yearElem = document.createElement("option");
+    yearElem.value = currentYear
+    yearElem.textContent = currentYear;
+    selectYear.append(yearElem);
+    currentYear--;
+  }
+
+  for (var m = 0; m < 12; m++) {
+    let month = monthNames[m];
+    let monthElem = document.createElement("option");
+    monthElem.value = m;
+    monthElem.textContent = month;
+    selectMonth.append(monthElem);
+  }
+
+  var d = new Date();
+  var month = d.getMonth();
+  var year = d.getFullYear();
+  var day = d.getDate();
+
+  selectYear.val(year);
+  selectYear.on("change", AdjustDays);
+  selectMonth.val(month);
+  selectMonth.on("change", AdjustDays);
+
+  AdjustDays();
+  selectDay.val(day)
+
+  function AdjustDays() {
+    var year = selectYear.val();
+    var month = parseInt(selectMonth.val()) + 1;
+    selectDay.empty();
+
+    //get the last day, so the number of days in that month
+    var days = new Date(year, month, 0).getDate();
+
+    //lets create the days of that month
+    for (var d = 1; d <= days; d++) {
+      var dayElem = document.createElement("option");
+      dayElem.value = d;
+      dayElem.textContent = d;
+      selectDay.append(dayElem);
+    }
+  }
+});
+
+
+$(document).ready(function(){
+  $('.frequency').on('change', function() {
+    if ( this.value == '0')
+    {
+      var src = document.getElementById("startDate"),
+      dst0 = document.getElementById("year");
+      dst1 = document.getElementById("month");
+      dst = "01/"+dst1+"/"+dst0
+
+      src.addEventListener('input', function() {
+          src.value = dst.value;
+      });
+    }
+  });
+});
--- vismet/static/vismet/js/datepicker.js
+++ vismet/static/vismet/js/datepicker.js
@@ -1,46 +1,4 @@
-$(function() {
-     $('.month-picker').datepicker(
-                    {
-                        dateFormat: "mm/yy",
-                        changeMonth: true,
-                        changeYear: true,
-                        showButtonPanel: true,
-                        todayBtn: false,
-                        closeText: 'Selecionar',
-                        onClose: function(dateText, inst) {
-
-
-                            function isDonePressed(){
-                                return ($('#ui-datepicker-div').html().indexOf('ui-datepicker-close ui-state-default ui-priority-primary ui-corner-all ui-state-hover') > -1);
-                            }
-
-                            if (isDonePressed()){
-                                var month = $("#ui-datepicker-div .ui-datepicker-month :selected").val();
-                                var year = $("#ui-datepicker-div .ui-datepicker-year :selected").val();
-                                $(this).datepicker('setDate', new Date(year, month, 1)).trigger('change');
-                                
-                                 $('.month-picker').focusout()//Added to remove focus from datepicker input box on selecting date
-                            }
-                        },
-                        beforeShow : function(input, inst) {
-                            setTimeout(function(){
-                            $(input).datepicker("widget").find(".ui-datepicker-current").hide();
-                            })
-                            inst.dpDiv.addClass('month_year_datepicker')
-
-                            if ((datestr = $(this).val()).length > 0) {
-                                year = datestr.substring(datestr.length-4, datestr.length);
-                                month = datestr.substring(0, 2);
-                                $(this).datepicker('option', 'defaultDate', new Date(year, month-1, 1));
-                                $(this).datepicker('setDate', new Date(year, month-1, 1));
-                                $(".ui-datepicker-calendar").hide();
-                            }
-                        }
-                    })
-});
-
-$(function() {
-    $(".dateinput").datepicker({
+$(".dateinput").datepicker({
     showOn: 'focus',
     dateFormat: 'dd/mm/yy',
     dayNames: ['Domingo','Segunda','Terça','Quarta','Quinta','Sexta','Sábado'],
@@ -51,16 +9,7 @@ $(function() {
     nextText: 'Próximo',
     prevText: 'Anterior',
     showOtherMonths: true,
-    showButtonPanel: true,
-    closeText: 'Selecionar',
-    currentText: 'Hoje',
     todayBtn: false,
     changeYear: true,
     changeMonth: true,
-    beforeShow : function(input, inst) {
-        setTimeout(function(){
-        $(input).datepicker("widget").find(".ui-datepicker-current").hide();
-        })
-    }
     });
-});
