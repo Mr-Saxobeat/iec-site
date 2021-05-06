53c083b163e2260b605ffa652dc490d003d9e2dc
Weiglas <weiglasribeiro@gmail.com>
2021-05-06 18:00:16 -0300

    Corrige cast do ALLOWED_HOSTS

--- conf/settings.py
+++ conf/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default='False', cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
 
 USE_PRODUCTION_API = config('USE_PRODUCTION_API', default=False, cast=bool)
 
